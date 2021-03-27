3596a9474fa6aedf00f5c8d1e81252786625d755
Mykola <afagus.13@gmail.com>
2021-03-27 09:31:21 +0200

    Home work, Made some correction. Made media query.

---
+++ js/script.js
@@ -0,0 +1,8 @@
+
+let btnClick = document.getElementById('onClick');
+let partnerIcons = document.getElementById('partners')
+
+btnClick.addEventListener('click', function () {
+    partnerIcons.style.display='none';
+
+})
