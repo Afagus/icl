df6b2ba5baa2cc1493ded88c5f9dac6354703194
Nick <afagu.13@gmail.com>
2021-03-27 18:59:19 +0200

    class

--- js/script.js
+++ js/script.js
@@ -1,8 +1,58 @@
-
 let btnClick = document.getElementById('onClick');
-let partnerIcons = document.getElementById('partners')
+let partnerIcons = document.querySelectorAll('.partner-icon')
+var btn = document.createElement("button");
+var bodyPartners = document.getElementById("partners");
 
 btnClick.addEventListener('click', function () {
-    partnerIcons.style.display='none';
+    for (var partner of partnerIcons) {
+        partner.style.display = 'none';
+    }
 
+    btn.className = "partnerShowBtn";
+    btn.innerText = "Show partner";
+    document.getElementById("partnersMain").append(btn);
+    this.style.display = "none";
+});
+
+
+function addImage(n) {
+    for (var arrayOfPictures = [], image, i = 1; i <= n; i++) {
+        image = new Image;
+        image.className = "partner-icon";
+        image.src = "img/partners/partner" + i + ".png";
+
+        arrayOfPictures.push(image);
+    }
+    return arrayOfPictures;
+}
+
+
+let arrayOfPartners = addImage(10);
+
+var counter = 0;
+btn.addEventListener("click", function () {
+    for (let i = 0; i < 3; i++) {
+        setTimeout(temp, 2000 * (i +1))
+    }
 })
+
+
+function temp() {
+    var div = document.createElement("div");
+    div.className = "partner-item";
+    var a = document.createElement("a");
+    a.innerHTML = arrayOfPartners[counter++].outerHTML;
+    div.prepend(a);
+    bodyPartners.prepend(div);
+
+    //console.log(alert(counter));
+
+}
+
+
+
+
+
+
+
+
