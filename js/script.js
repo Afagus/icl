dff507441c160059f8593bf6c67e69f06db33793
favorit <afagus.13@gmail.com>
2021-04-06 11:49:50 +0300

    Заработало!

--- js/script.js
+++ js/script.js
@@ -2,6 +2,10 @@ let btnClick = document.getElementById('onClick');
 let partnerIcons = document.querySelectorAll('.partner-icon')
 var btn = document.createElement("button");
 var bodyPartners = document.getElementById("partners");
+var arrayFromFile;
+var arrayOfPartners = Array();
+var arrayOfPictures = Array();
+
 
 btnClick.addEventListener('click', function () {
     for (var partner of partnerIcons) {
@@ -15,46 +19,43 @@ btnClick.addEventListener('click', function () {
 });
 
 
-let xhr = new XMLHttpRequest()
-let url = "js/array.json"
+let xhr = new XMLHttpRequest();
+let url = "js/array.json";
 
 
 xhr.onreadystatechange = function () {
-    if (this.status == 200 && this.readyState == 4) {
-        var arrayFromFile = JSON.stringify(this.response)
-        // document.getElementById("demo").innerText = arrayFromFile
-        var arrayOfPartners = addImage(arrayFromFile);
+    if (this.status === 200 && this.readyState === 4) {
+        arrayFromFile = this.response;
+
+        arrayOfPartners = addImage(arrayFromFile);
+        console.log(arrayOfPartners);
     }
 }
 xhr.open("GET", url, true)
 xhr.responseType = 'json';
-xhr.send()
+xhr.send();
 
 
 function addImage(arrayFromOutside) {
-    arrayFromOutside.forEach(function () {
+    arrayFromOutside.forEach(function (item) {
         {
             var image = new Image;
             image.className = item.className;
             image.src = item.src;
             image.alt = item.alt
-            document.getElementById("demo").innerText = image.src
             arrayOfPictures.push(image);
         }
 
-})
+    })
 
 
 }
 
 
-
-
-
 var counter = 0;
 btn.addEventListener("click", function () {
     for (let i = 0; i < 3; i++) {
-        setTimeout(temp, 2000 * (i + 1))
+        setTimeout(temp, 2000 * (i + 1));
     }
 })
 
@@ -63,7 +64,7 @@ function temp() {
     var div = document.createElement("div");
     div.className = "partner-item";
     var a = document.createElement("a");
-    a.innerHTML = arrayOfPartners[counter++].outerHTML;
+    a.innerHTML = arrayOfPictures[counter++].outerHTML;
     div.prepend(a);
     bodyPartners.prepend(div);
 
